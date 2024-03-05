8f17d134f4907cc928b02b703ecc1579a8bf7996
Arshad Shaikh <Arshad@nichetech.in>
2024-03-05 19:01:11 +0530

    bodyparser and more

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-const http=require('http');
+const http = require('http');
 
-const express=require('express');
+const bodyParser=require('body-parser')
+const express = require('express');
 
-const app=express();
+const app = express();
 
-app.use((req,res,next)=>{
-    console.log('we are in the middleware!');
-    next();
+// app.use('/', (req, res, next) => {
+//     console.log('this will always run!')
+//     next();
+// });
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="Product-name"><br><button type="submit">Add Product</button></form>');
+
 });
+app.use('/product',(req,res)=>{
+    console.log(req.body)
+    res.redirect('/')
+});
+app.use('/', (req, res, next) => {
+    res.send('<h1>you are at the Root</h1>');
 
-app.use((req,res,next)=>{
-    console.log('we are in the middleware!');
 });
 
 app.listen(3000);
